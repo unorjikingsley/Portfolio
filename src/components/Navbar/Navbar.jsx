290456e44af136c206ed27781fa852739ce59c2d
unorji kingsley <unorjikingsley@gmail.com>
2023-10-13 18:03:28 +0100

    Modify NavBar with background feature

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,51 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-scroll';
 import Toggle from '../Toggle/Toggle';
 import './Navbar.css';
+import { themeContext } from '../../Context';
 
-const navbar = () => (
-  <div className="n-wrapper" id="Navbar">
-    {/* left */}
-    <div className="n-left">
-      <div className="n-name">Kingsley</div>
-      <Toggle />
-    </div>
-    {/* right */}
-    <div className="n-right">
-      <div className="n-list">
-        <ul style={{ listStyleType: 'none' }}>
-          <li>
-            <Link activeClass="active" to="Navbar" spy smooth>
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="services" spy smooth>
-              Serivces
-            </Link>
-          </li>
-          <li>
-            <Link to="works" spy smooth>
-              Technology
-            </Link>
-          </li>
-          <li>
-            <Link to="portfolio" spy smooth>
-              Portfolio
-            </Link>
-          </li>
-          <li>
-            <Link to="testimonial" spy smooth>
-              Testimonial
-            </Link>
-          </li>
-        </ul>
+const Navbar = () => {
+  const theme = useContext(themeContext);
+  const { darkMode } = theme.state;
+
+  // Define styles for the navbar based on dark mode
+  const navbarStyle = {
+    background: darkMode ? 'black' : 'white',
+    // Add other styling properties as needed
+  };
+
+  return (
+    <div className="n-wrapper" id="Navbar" style={navbarStyle}>
+      {/* left */}
+      <div className="n-left">
+        <div className="n-name">Kingsley</div>
+        <Toggle />
       </div>
-      <Link to="contact" spy smooth>
-        <button type="button" className="button n-button">Contact</button>
-      </Link>
+      {/* right */}
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: 'none' }}>
+            <li>
+              <Link activeClass="active" to="Navbar" spy smooth>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="services" spy smooth>
+                Services
+              </Link>
+            </li>
+            <li>
+              <Link to="works" spy smooth>
+                Technology
+              </Link>
+            </li>
+            <li>
+              <Link to="portfolio" spy smooth>
+                Portfolio
+              </Link>
+            </li>
+            <li>
+              <Link to="testimonial" spy smooth>
+                Testimonial
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <Link to="contact" spy smooth>
+          <button type="button" className="button n-button">
+            Contact
+          </button>
+        </Link>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default navbar;
+export default Navbar;
